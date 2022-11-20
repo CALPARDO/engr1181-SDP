af05c566a5a3e0316e140dba2399e7a336b09dd0
Jane1508 <118584312+Jane1508@users.noreply.github.com>
2022-11-20 17:25:38 -0500

    Update computerguess.m

--- computerguess.m
+++ computerguess.m
@@ -1,6 +1,35 @@
+battleship_scene = simpleGameEngine('Battleship.png',84,84);
+
+%variables for sprites
+blank_s = 1;    %Blank sprite (black line to divide two sides of the board)
+water_s = 2;    %Water sprite (water scene)
+left_ship_s = 3;    %left side of ship sprite (left end of ship)
+horizontal_ship_s = 4;  %horizontal ship sprite
+right_ship_s = 5;   %right side of ship sprite (right end of ship)
+top_ship_s = 6;     %top of ship sprite
+vertical_ship_s = 7;    %vertical ship sprite
+bottom_ship_s = 8;  %bottom of ship sprite
+hit_s = 9;      %hit sprite (red dot; shows when ship is hit)
+miss_s = 10;    %miss sprite (white dot; shows when ship is missed)
+
+horizontalship5 = [3, 4, 4, 4, 5];
+horizontalship4 = [3, 4, 4, 5];
+horizontalship3 = [3, 4, 5];
+horizontalship2 = [3, 5];
+
+verticalship5 = [6; 7; 7; 7; 8];
+verticalship4 = [6; 7; 7; 8];
+verticalship3 = [6; 7; 8];
+verticalship2 = [6; 8];
+
+%display empty board
+battleship_display = water_s * ones(10,21);
+battleship_display(:,11) = blank_s;
+drawScene(battleship_scene,battleship_display)
+
 %Computer Guessing where ships are
-%column = randi[12 21]
-%row = randi[4 13] (change for actual game)
+%column = randi[11 21]
+%row = randi[1 10] (change for actual game)
 %Board size of where computer is randomly guessing a point
 boardArray = reshape(board_display.',1,[]);
 s = 0;  %variable s = 16; the number of sprites containing a part of the ship                    
@@ -11,7 +40,7 @@ while s <= 16   %while the number of cells containing a point with a red dot is 
     %if the board display already has a dot, another point will be guessed
     %if the board display had a ship, then the board will display a red dot
     %This will add 1 to the variable s, taking away from the total number of sprites with ships
-    [c] = randi([111,200],1,1);
+    [c] = randi([110,200],1,1);
     %if the point guessed is the sprite containing the left side of the ship 
     if board_display(c) == 3
         board_display(c) = 9;   %display hit
