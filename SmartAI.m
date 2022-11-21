ae3084f3cfa05552ec8abea0cde1435e4435938d
Jane1508 <118584312+Jane1508@users.noreply.github.com>
2022-11-21 09:34:55 -0500

    Add files via upload

--- SmartAI.m
+++ SmartAI.m
@@ -77,109 +77,108 @@ for N = 1:1000
                 drawScene(BtlShipScn,NewBoard)
             end
         elseif CM ~= 1
-           if s <= 16
-    if NewBoard(c) ~= 9
-        [c] = randi([111,200],1,1)
-        if NewBoard(c) == left_ship_s || NewBoard(c) == horizontal_ship_s || NewBoard(c) == right_ship_s || NewBoard(c) == top_ship_s || NewBoard(c) == vertical_ship_s ||  NewBoard(c) == bottom_ship_s
-        NewBoard(c) = hit_s;
-        s = s + 1;
-        C = C + 1 
-        %If the board is displaying a water sprite, then when it is hit, a white dot will be displayed in that spot.
-        elseif NewBoard(c) == water_s
-        NewBoard(c) = miss_s;
-        C = C + 1;
-        elseif NewBoard(c) == miss_s
-        [c] = randi([111, 200],1,1)
-        elseif NewBoard(c) == 1
-        [c] = randi([111, 200],1,1)
-        end
-        drawScene(BtlShipScn,NewBoard)
-        end
-    elseif NewBoard(c) == 9
-    if ComputersShip(c) == 3         %%if hit on the left side of the ship
-        [c] = NewBoard(c) + 10;       %computer guesses the cell to the right side
-        %will either be 4 or 5
-        if NewBoard(c) == 4
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        elseif NewBoard(c) == 5
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        end
-    elseif ComputersShip(c) == 4     %%if hit on a horizontal piece 
-        [c] = NewBoard + 10;       %computer guesses the cell to the right
-        %will either be 4 or 5
-        if NewBoard(c) == 4
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        elseif NewBoard(c) == 5
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        end
-    elseif ComputersShip(c) == 5     %%if hit is on the right side of the ship
-        [c] = NewBoard - 10;       %computer guesses the cell to the left
-        %will either be 3 or 4
-        if NewBoard(c) == 3
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        elseif NewBoard(c) == 4
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        end
-    elseif ComputersShip(c) == 6     %%if hit is on the top
-        [c] = bboard_display(c) - 1;     %computer guesses cell below
-        %will either be 7 or 8
-        if NewBoard(c) == 7
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        elseif NewBoard(c) == 8
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        end
-    elseif ComputersShip(c) == 7     %%if hit is on the vertical piece
-        [c] = NewBoard(c) - 1;     %computer guesses the cell below
-        %will either be 7 or 8
-        if NewBoard(c) == 7
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        elseif NewBoard(c) == 8
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        end
-    elseif ComputersShip(c) == 8     %%if the hit is on the bottom of the ship
-        [c] = NewBoard(c) + 1;     % computer guesses one cell above
-        %will either be 6 or 7
-        if NewBoard(c) == 7
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        elseif NewBoard(c) == 8
-        NewBoard(c) = 9;
-        s = s + 1;
-        C = C + 1;
-        end
-    %else battleship_scene(l) = 4; %%if hit is still on horizontal piece
-        %[c] = randi([111,200],1,1);   %guesses two to the left
-    %else battleship_scene(l) == 7 %%if hit is still on the vertical piece 
-        %[c] = randi([111,200],1,1);    %guesses two to up
-    else                                % or if any other situation occurs
-         [c] = randi([111, 200],1,1)   %goes back to original while loop
-    end
-    drawScene(battleship_scene,NewBoard)
+            if NewBoard(c) == hit_s
+                if PlayersShips(c) == 3         %%if hit on the left side of the ship
+                [c] = NewBoard(c) + 10;       %computer guesses the cell to the right side
+                %will either be 4 or 5
+                    if NewBoard(c) == 4
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    elseif NewBoard(c) == 5
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    end
+                elseif PlayersShips(c) == 4     %%if hit on a horizontal piece 
+                [c] = NewBoard + 10;       %computer guesses the cell to the right
+                %will either be 4 or 5
+                    if NewBoard(c) == 4
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    elseif NewBoard(c) == 5
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    end
+                elseif PlayersShips(c) == 5     %%if hit is on the right side of the ship
+                [c] = NewBoard - 10;       %computer guesses the cell to the left
+                %will either be 3 or 4
+                    if NewBoard(c) == 3
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    elseif NewBoard(c) == 4
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    end
+                elseif PlayersShips(c) == 6     %%if hit is on the top
+                [c] = board_display(c) - 1;     %computer guesses cell below
+                %will either be 7 or 8
+                    if NewBoard(c) == 7
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    elseif NewBoard(c) == 8
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    end
+                elseif PlayersShips(c) == 7     %%if hit is on the vertical piece
+                [c] = NewBoard(c) - 1;     %computer guesses the cell below
+                %will either be 7 or 8
+                    if NewBoard(c) == 7
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    elseif NewBoard(c) == 8
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    end
+                elseif PlayersShips(c) == 8     %%if the hit is on the bottom of the ship
+                [c] = NewBoard(c) + 1;     % computer guesses one cell above
+                %will either be 6 or 7
+                    if NewBoard(c) == 7
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    elseif NewBoard(c) == 8
+                    NewBoard(c) = 9;
+                    s = s + 1;
+                    C = C + 1;
+                    end
+                %else battleship_scene(l) = 4; %%if hit is still on horizontal piece
+                %[c] = randi([111,200],1,1);   %guesses two to the left
+                %else battleship_scene(l) == 7 %%if hit is still on the vertical piece 
+                %[c] = randi([111,200],1,1);    %guesses two to up
+                else                                % or if any other situation occurs
+                [c] = randi([111, 200],1,1)   %goes back to original while loop
+                end
+                drawScene(battleship_scene,NewBoard)
+            elseif NewBoard(c) ~= hit_s
+            [c] = randi([111,200],1,1)
+                if NewBoard(c) == left_ship_s || NewBoard(c) == horizontal_ship_s || NewBoard(c) == right_ship_s || NewBoard(c) == top_ship_s || NewBoard(c) == vertical_ship_s ||  NewBoard(c) == bottom_ship_s
+                NewBoard(c) = hit_s;
+                s = s + 1;
+                C = C + 1 
+                %If the board is displaying a water sprite, then when it is hit, a white dot will be displayed in that spot.
+                elseif NewBoard(c) == water_s
+                NewBoard(c) = miss_s;
+                C = C + 1;
+                elseif NewBoard(c) == miss_s
+                [c] = randi([111, 200],1,1);
+                elseif NewBoard(c) == 1
+                [c] = randi([111, 200],1,1);
+                end
+                drawScene(BtlShipScn,NewBoard)
                 end
             end
         end
-    end
+end
+
 
 
 
