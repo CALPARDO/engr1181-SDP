3b3c7a4401a8385ae691683d4b45b3981b0332fc
Derin Durak <durakderin@gmail.com>
2022-11-30 02:52:53 -0500

    PVP and CPU cases are added

--- menu.m
+++ menu.m
@@ -1,19 +1,22 @@
 function menu(scene_menu)
-    sfx_player = sfx('menu');
+    sfx_player = sfx('game');
     drawScene(scene_menu, 1);
     while 1
+        if isplaying(sfx_player) == 0; play(sfx_player); end
         [r, c, b] = ginput(1);
-        if r >= 459 && r <= 620 && c >= 394 && r <= 555 && b == 1
-            stop(sfx_player);
-            close all
-            return;
-        end
-        %{
-         elseif r >= 247 && r <= 355 && c >= 557 && r <= 668 && b == 1
+        if r >= 225 && r <= 386 && c >= 395 && c <= 556 && b == 1
+            %stop(sfx_player);
+            %close all
+            %cpu();
+            fprintf('CPU\n');
+            %return;
+        elseif r >= 706 && r <= 865 && c >= 395 && c <= 556 && b == 1
             if isplaying(sfx_player) == 0; play(sfx_player); end
-            close all
-            game_set();
+            %close all
+            %pvp();
+            fprintf('PVP\n');
+        else
+            fprintf('idiot\n');
         end
-        %}
     end
 end
